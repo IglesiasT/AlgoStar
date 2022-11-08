36edaea8f0d9b59d8cbc6833a528a35103ee75fc
Tomás <iglesiastomas99@gmail.com>
2022-11-08 20:35:22 -0300

    Minimo codigo Extractor para test

---
+++ src/main/java/edu/fiuba/algo3/modelo/Extractor.java
@@ -0,0 +1,19 @@
+package edu.fiuba.algo3.modelo;
+
+public class Extractor {
+    private int turnos;
+    public Extractor(){
+        this.turnos = 0;
+    }
+
+    public void nuevoTurno(){
+        this.turnos++;
+    }
+
+    public void obtenerGasProducido() throws EdificioNoEstaOperativo{
+        if (this.turnos < 6){
+            throw EdificioNoEstaOperativo;
+        }
+
+    }
+}
