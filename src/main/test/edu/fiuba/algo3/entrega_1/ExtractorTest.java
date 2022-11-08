f839da7a69a0070f110a8f8e87732d76d8ae9a7a
Tomás <iglesiastomas99@gmail.com>
2022-11-08 20:27:44 -0300

    Test turnos construccion extractor

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/ExtractorTest.java
@@ -0,0 +1,22 @@
+package edu.fiuba.algo3.entrega_1;
+
+import edu.fiuba.algo3.modelo.EdificioNoEstaOperativo;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExtractorTest {
+
+    @Test
+    public void extractorNoEstaOperativoAntesDeSeisTurnos(){
+        //Arrange
+        Extractor extractor = new Extractor();
+
+        //Act
+        extractor.nuevoTurno();
+        extractor.nuevoTurno();
+        extractor.nuevoTurno();
+
+        //Assert
+        assertThrows(EdificioNoEstaOperativo.class, extractor::obtenerGasProducido());
+    }
+}
