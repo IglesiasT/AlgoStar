06931dc6b46f55e7b254b3e1391d953827fc8f17
Tomás <iglesiastomas99@gmail.com>
2022-11-15 17:23:20 -0300

    primer planeo caso 18

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso18Test.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.modelo.construcciones.NexoMineral;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CasoDeUso18Test {
+    // Unidades Zerg
+    @Test
+    public void zerlingInflinge4DeDanioEnTierra(){
+        // Arrange
+        Zerling zerling = new Zerling();
+        NexoMineral nexoMineral = new NexoMineral();
+        int valorEsperado = 246;    //250 escudo - 4 ataque
+
+        // Act
+        zerling.atacar(nexoMineral);
+
+        // Assert
+        assertEquals(valorEsperado, nexoMineral.obtenerEscudo());
+    }
+}
