0875140d35cae99ad048d236243c5feeeca22196
Tomás <iglesiastomas99@gmail.com>
2022-11-15 18:26:46 -0300

    add hidralisco

---
+++ src/main/java/edu/fiuba/algo3/modelo/construcciones/unidades/Hidralisco.java
@@ -0,0 +1,20 @@
+package edu.fiuba.algo3.modelo.construcciones.unidades;
+
+import edu.fiuba.algo3.modelo.tablero.Casillero;
+
+public class Hidralisco extends UnidadZerg{
+    public Hidralisco(){
+        this.danioBase = 10;
+        this.vida = 80;
+        this.turnosParaConstruirse = 4;
+        this.rangoDeAtaque = 4;
+        this.superficie = "Tierra";
+        this.mineralNecesarioParaConstruir = 75;
+        this.gasNecesarioParaConstruir = 25;
+    }
+
+    @Override
+    public boolean sePuedeConstruirEn(Casillero casillero) {
+        return false;
+    }
+}
