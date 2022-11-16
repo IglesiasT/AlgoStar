65731f06bca886fb92ead1b6b7ecff12ac3738b0
Tomás <iglesiastomas99@gmail.com>
2022-11-16 20:04:32 -0300

    add excepcion objetivo invalido

--- src/main/java/edu/fiuba/algo3/modelo/construcciones/unidades/UnidadZerg.java
+++ src/main/java/edu/fiuba/algo3/modelo/construcciones/unidades/UnidadZerg.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo.construcciones.unidades;
 
+import edu.fiuba.algo3.modelo.ObjetivoInvalido;
 import edu.fiuba.algo3.modelo.construcciones.ConstruccionProtoss;
 import edu.fiuba.algo3.modelo.construcciones.ConstruccionZerg;
 
@@ -12,6 +13,9 @@ public abstract class UnidadZerg extends ConstruccionZerg {
     protected int rangoDeAtaque;
 
     public void atacar(ConstruccionProtoss construccionEnemiga){
+        if (!this.danioPorSuperficie.containsKey(construccionEnemiga.obtenerSuperficie())){
+            throw new ObjetivoInvalido();
+        }
         int danio = this.danioPorSuperficie.get(construccionEnemiga.obtenerSuperficie());
         construccionEnemiga.recibirDanio(danio);
     }
