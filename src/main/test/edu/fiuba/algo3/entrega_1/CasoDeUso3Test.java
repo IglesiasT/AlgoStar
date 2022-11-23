4d17b0a212ec10b63bc816ab8a391045ec29b0e9
Tomás <iglesiastomas99@gmail.com>
2022-11-23 19:22:57 -0300

    fix tests caso 3 post refactor

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso3Test.java
@@ -4,46 +4,75 @@ import edu.fiuba.algo3.modelo.areas.AreaTerrestre;
 import edu.fiuba.algo3.modelo.construcciones.*;
 import edu.fiuba.algo3.modelo.espaciosDeConstruccion.Moho;
 import edu.fiuba.algo3.modelo.espaciosDeConstruccion.RangoPilon;
+import edu.fiuba.algo3.modelo.mapa.CasilleroSinGas;
 import edu.fiuba.algo3.modelo.recursos.Gas;
 import edu.fiuba.algo3.modelo.mapa.Casillero;
 import edu.fiuba.algo3.modelo.mapa.Mapa;
+import edu.fiuba.algo3.modelo.recursos.ListadoDeRecursos;
+import edu.fiuba.algo3.modelo.recursos.Mineral;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CasoDeUso3Test {
     @Test
     public void asimiladorSePuedeConstruirEnCasilleroConGas() {
+        // Arrange
         Asimilador asimilador = new Asimilador();
-
         Casillero casillero = new Casillero(new Gas(),new AreaTerrestre(), 1, 1, new Mapa());
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        assert(asimilador.sePuedeConstruirEn(casillero));
+        // Act
+        recursos.agregar(new Mineral());
+        asimilador.construir(casillero, recursos);
+
+        // Assert
+        assertEquals(Asimilador.class, casillero.obtenerConstruccion().getClass());
     }
 
     @Test
     public void asimiladorNoSePuedeConstruirEnCasilleroSinGas() {
+        // Arrange
         Asimilador asimilador = new Asimilador();
+        Casillero casillero = new Casillero(new AreaTerrestre(), 1, 1, new Mapa());
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        Casillero casillero = new Casillero(new AreaTerrestre(),1, 1, new Mapa());
+        // Act
+        recursos.agregar(new Mineral());
 
-        assert(!asimilador.sePuedeConstruirEn(casillero));
+        // Assert
+        assertThrows(CasilleroSinGas.class, () -> asimilador.construir(casillero, recursos));
     }
 
     @Test
     public void extractorSePuedeConstruirEnUnCasilleroConGas(){
+        // Arrange
         Extractor extractor = new Extractor();
         Casillero casillero = new Casillero(new Gas(),new AreaTerrestre(), 1, 1, new Mapa());
-        casillero.setEspacioDeConstruccion(new Moho());
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        assert(extractor.sePuedeConstruirEn(casillero));
+        // Act
+        recursos.agregar(new Mineral());
+        casillero.setEspacioDeConstruccion(new Moho());
+        extractor.construir(casillero, recursos);
+
+        // Assert
+        assertEquals(Extractor.class, casillero.obtenerConstruccion().getClass());
     }
 
     @Test
     public void extractorNoSePuedeConstruirEnUnCasilleroSinGas(){
+        // Arrange
         Extractor extractor = new Extractor();
-        Casillero casillero = new Casillero(new AreaTerrestre(),1, 1, new Mapa());
-        casillero.setEspacioDeConstruccion(new Moho());
+        Casillero casillero = new Casillero(new AreaTerrestre(), 1, 1, new Mapa());
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        assert(!extractor.sePuedeConstruirEn(casillero));
+        // Act
+        recursos.agregar(new Mineral());
+
+        // Assert
+        assertThrows(CasilleroSinGas.class, () -> extractor.construir(casillero, recursos));
     }
 
     @Test
