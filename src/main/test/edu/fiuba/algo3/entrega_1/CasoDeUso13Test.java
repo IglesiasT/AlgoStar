a54c96026f88ff97029ec0ad10405386da46504d
Tomás <iglesiastomas99@gmail.com>
2022-11-25 23:29:25 -0300

    fix test caso 13

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso13Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso13Test.java
@@ -2,22 +2,26 @@ package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.construcciones.Criadero;
 import edu.fiuba.algo3.modelo.construcciones.ReservaDeReproduccion;
+import edu.fiuba.algo3.modelo.espaciosDeConstruccion.Moho;
 import edu.fiuba.algo3.modelo.mapa.Casillero;
 import edu.fiuba.algo3.modelo.mapa.Mapa;
+import edu.fiuba.algo3.modelo.recursos.ListadoDeRecursos;
+import edu.fiuba.algo3.modelo.recursos.Mineral;
 import org.junit.jupiter.api.Test;
 
 public class CasoDeUso13Test {
 
     @Test
-    public void seDestrulleCriaderoPeroElMohoQueda(){
-        //Arrange
+    public void seDestruyeCriaderoPeroElMohoQueda(){
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero1 = mapa.obtenerCasillero(1,1);
         Criadero criadero = new Criadero();
-        ReservaDeReproduccion reserva = new ReservaDeReproduccion();
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        //Act
-        criadero.construir(casillero1);
+        // Act
+        recursos.agregar(new Mineral());
+        criadero.construir(casillero1, recursos);
         criadero.nuevoTurno();
         criadero.nuevoTurno();
         criadero.nuevoTurno();
@@ -27,7 +31,7 @@ public class CasoDeUso13Test {
         criadero.destruir();
         criadero.nuevoTurno();
 
-        //Assert
-        //assert reserva.sePuedeConstruirEn(mapa.obtenerCasillero(1,3));
+        // Assert
+        assert mapa.obtenerCasillero(1,3).contiene(new Moho());
     }
 }
