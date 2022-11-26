93939e7ceea0e9da4aa4067d81b8ad1cb2c4fff9
Tomás <iglesiastomas99@gmail.com>
2022-11-25 23:46:16 -0300

    fix tests caso 14

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso14Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso14Test.java
@@ -1,64 +1,66 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.construcciones.Acceso;
 import edu.fiuba.algo3.modelo.construcciones.Criadero;
 import edu.fiuba.algo3.modelo.construcciones.Pilon;
-import edu.fiuba.algo3.modelo.construcciones.ReservaDeReproduccion;
+import edu.fiuba.algo3.modelo.espaciosDeConstruccion.Moho;
+import edu.fiuba.algo3.modelo.espaciosDeConstruccion.RangoPilon;
 import edu.fiuba.algo3.modelo.mapa.Casillero;
 import edu.fiuba.algo3.modelo.mapa.Mapa;
+import edu.fiuba.algo3.modelo.recursos.ListadoDeRecursos;
+import edu.fiuba.algo3.modelo.recursos.Mineral;
 import org.junit.jupiter.api.Test;
 
 public class CasoDeUso14Test {
 
     @Test
     public void pilonNoEnergizaCasillerosConMoho(){
-        //Arrange
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero1 = mapa.obtenerCasillero(1,1);
         Casillero casillero2 = mapa.obtenerCasillero(1,7);
         Criadero criadero = new Criadero();
         Pilon pilon = new Pilon();
-        Acceso acceso = new Acceso();
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        //Act
-        criadero.construir(casillero1);
+        // Act
+        recursos.agregar(new Mineral());
+        criadero.construir(casillero1, recursos);
         criadero.nuevoTurno();
         criadero.nuevoTurno();
         criadero.nuevoTurno();
         criadero.nuevoTurno();
 
-        pilon.construir(casillero2);
+        pilon.construir(casillero2, recursos);
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
 
-
-
-        //Assert
-//        assert !acceso.sePuedeConstruirEn(mapa.obtenerCasillero(1,4));
+        // Assert
+        assert !(mapa.obtenerCasillero(1,4).contiene(new RangoPilon()));
     }
 
     @Test
     public void mohoCreceAunEstandoEnElRangoDeUnPilon(){
-        //Arrange
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero1 = mapa.obtenerCasillero(1,1);
         Casillero casillero2 = mapa.obtenerCasillero(1,7);
         Criadero criadero = new Criadero();
         Pilon pilon = new Pilon();
-        ReservaDeReproduccion reserva = new ReservaDeReproduccion();
+        ListadoDeRecursos recursos = new ListadoDeRecursos();
 
-        //Act
-        pilon.construir(casillero1);
+        // Act
+        recursos.agregar(new Mineral());
+        criadero.construir(casillero1, recursos);
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
         pilon.nuevoTurno();
 
-        criadero.construir(casillero2);
+        criadero.construir(casillero2, recursos);
         criadero.nuevoTurno();
         criadero.nuevoTurno();
         criadero.nuevoTurno();
@@ -67,7 +69,7 @@ public class CasoDeUso14Test {
         criadero.nuevoTurno();
 
 
-        //Assert
-//        assert reserva.sePuedeConstruirEn(mapa.obtenerCasillero(1,2));
+        // Assert
+        assert mapa.obtenerCasillero(1,2).contiene(new Moho());
     }
 }
