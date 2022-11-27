10428c7783c8160db6d16503d2a227819e907384
Tomás <iglesiastomas99@gmail.com>
2022-11-26 22:20:48 -0300

    fix tests caso 17

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso17Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso17Test.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.*;
+import edu.fiuba.algo3.modelo.construcciones.ConstruccionPreviaNoConstruida;
 import edu.fiuba.algo3.modelo.espaciosDeConstruccion.Moho;
 import edu.fiuba.algo3.modelo.espaciosDeConstruccion.RangoPilon;
 import edu.fiuba.algo3.modelo.razas.Protoss;
@@ -15,46 +16,47 @@ public class CasoDeUso17Test {
 
     @Test
     public void guaridaNoSePuedeConstruirSinReservaDeReproduccion() {
-
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero1 = mapa.obtenerCasillero(1,1);
-
-
         Zerg razaZerg = new Zerg(1000,1000);
 
+        // Act
         casillero1.setEspacioDeConstruccion(new Moho());
 
-        assertThrows(NoSePuedeConstruir.class, () -> razaZerg.construirGuarida(casillero1) );
+        // Assert
+        assertThrows(ConstruccionPreviaNoConstruida.class, () -> razaZerg.construirGuarida(casillero1) );
     }
 
     @Test
     public void espiralNoSePuedeConstruirSinGuarida() {
-
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero1 = mapa.obtenerCasillero(1,1);
         Casillero casillero2 = mapa.obtenerCasillero(1,2);
-
-
         Zerg razaZerg = new Zerg(1000,1000);
 
+        // Act
         casillero1.setEspacioDeConstruccion(new Moho());
         casillero2.setEspacioDeConstruccion(new Moho());
-
         razaZerg.construirReservaDeReproduccion(casillero1);
 
-        assertThrows(NoSePuedeConstruir.class, () -> razaZerg.construirEspiral(casillero2) );
+        // Assert
+        assertThrows(ConstruccionPreviaNoConstruida.class, () -> razaZerg.construirEspiral(casillero2) );
     }
 
     @Test
     public void puertoEstelarNoSePuedeConstruirSinAcceso(){
-
+        // Arrange
         Mapa mapa = new Mapa();
         Casillero casillero = mapa.obtenerCasillero(1,1);
-
         Protoss razaProtoss = new Protoss(1000,1000);
+
+        // Act
         casillero.setEspacioDeConstruccion(new RangoPilon());
 
-        assertThrows(NoSePuedeConstruir.class, () -> razaProtoss.construirPuertoEstelar(casillero) );
+        // Assert
+        assertThrows(ConstruccionPreviaNoConstruida.class, () -> razaProtoss.construirPuertoEstelar(casillero) );
     }
 
 }
