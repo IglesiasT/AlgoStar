0cf8c4988cc84466addb656084853ebc7175e000
Tomás <iglesiastomas99@gmail.com>
2022-11-28 19:43:32 -0300

    delete metodo obsoleto

--- src/main/java/edu/fiuba/algo3/modelo/construcciones/unidades/Unidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/construcciones/unidades/Unidad.java
@@ -4,6 +4,5 @@ import edu.fiuba.algo3.modelo.construcciones.Construccion;
 import edu.fiuba.algo3.modelo.mapa.Casillero;
 
 public interface Unidad {
-    void atacar(Construccion construccion);
     void moverse(Casillero casilleroDestino);
 }
